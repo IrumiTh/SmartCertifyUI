773ac59f2c1d6ddbf09be83bc9f9a1fa2dd01e09
irumi <irumiabeywickrama@gmail.com>
2025-12-05 22:59:53 +0530

    add home page to app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { AboutComponent } from './pages/about/about.component';
 import { HeaderComponent } from './pages/header/header.component';
 import { FooterComponent } from './pages/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, AboutComponent, HeaderComponent, FooterComponent],
+  imports: [RouterOutlet,  HeaderComponent, FooterComponent, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
