80ee9537c930e98e4e5437028a42847762d315c8
irumi <irumiabeywickrama@gmail.com>
2025-12-05 22:59:19 +0530

    complete about page

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-about',
   imports: [],
-  template:`<p>About SmartCertify</p>`,
+  templateUrl: './about.component.html',
   styleUrl: './about.component.css'
 })
 export class AboutComponent {
