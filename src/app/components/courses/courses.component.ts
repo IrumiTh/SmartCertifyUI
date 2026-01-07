9cf31fb4e0870fdf441cc99b1c266e30d10dec29
irumi <irumiabeywickrama@gmail.com>
2026-01-07 22:29:01 +0530

    implement courses service

--- src/app/components/courses/courses.component.ts
+++ src/app/components/courses/courses.component.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Course } from '../../models/course';
+import { CoursesService } from '../../services/courses.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-courses',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './courses.component.html',
   styleUrl: './courses.component.css'
 })
-export class CoursesComponent {
+export class CoursesComponent implements OnInit {
+  courses: Course[] = [];
 
+  constructor(private courseService: CoursesService){
+    
+  }
+  //for calling the loadCourses method when component is initialized
+  ngOnInit(): void {
+    this.loadCourses();
+  }
+
+  loadCourses(){
+      this.courseService.getCourses().subscribe(courses => {
+        this.courses = courses;
+      });
+  }
 }
--- src/app/services/courses.service.ts
+++ src/app/services/courses.service.ts
@@ -1,13 +1,15 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Course } from '../models/course';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CoursesService {
-  private baseurl = '';
+  private baseurl = 'https://localhost:7142/api';
   constructor(private http: HttpClient) { }
-  getCourses() {
-    return this.http.get<any>(this.baseurl + '/courses');
+  getCourses(): Observable<Course[]> {
+    return this.http.get<Course[]>(this.baseurl + '/Course');
   }
 }
