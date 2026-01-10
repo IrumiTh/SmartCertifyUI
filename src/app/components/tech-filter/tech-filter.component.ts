08de301d4f595f3f2a5ee423580ee03c494761f8
irumi <irumiabeywickrama@gmail.com>
2026-01-10 23:24:21 +0530

    implement tech component

--- src/app/components/tech-filter/tech-filter.component.ts
+++ src/app/components/tech-filter/tech-filter.component.ts
@@ -1,11 +1,18 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-tech-filter',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './tech-filter.component.html',
   styleUrl: './tech-filter.component.css'
 })
 export class TechFilterComponent {
-
+  techList = [
+    {name: 'Angular', image: 'assets/images/angular.png'},
+    {name: 'React', image: 'assets/images/react.png'},
+    {name: 'Azure', image: 'assets/images/azure.png'},
+    {name: '.NET Core', image: 'assets/images/dotnetcore.png'},
+    {name: 'Javascript', image: 'assets/images/javascript.png'},
+  ]
 }
